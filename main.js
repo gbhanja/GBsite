c6d2d99727bb0059b2844e46a4170ecb1626f99f
GB <gbquantum23@gmail.com>
2026-06-23 16:44:21 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+const toggle=document.getElementById("themeToggle");
+
+if(localStorage.getItem("theme")==="dark"){
+document.body.classList.add("dark");
+}
+
+toggle?.addEventListener("click",()=>{
+
+document.body.classList.toggle("dark");
+
+if(document.body.classList.contains("dark")){
+localStorage.setItem("theme","dark");
+}else{
+localStorage.setItem("theme","light");
+}
+
+});
