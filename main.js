899dc61c5ac18a553b1b6a43be9a91fb8acb6827
GB <gbquantum23@gmail.com>
2026-06-23 16:56:40 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,36 @@
+const container=document.getElementById("particles");
+
+for(let i=0;i<80;i++){
+
+const dot=document.createElement("div");
+
+dot.style.position="absolute";
+dot.style.width="3px";
+dot.style.height="3px";
+dot.style.borderRadius="50%";
+dot.style.background="white";
+
+dot.style.left=Math.random()*100+"%";
+dot.style.top=Math.random()*100+"%";
+
+dot.style.opacity=Math.random();
+
+dot.animate(
+[
+{transform:"translateY(0px)"},
+{transform:"translateY(-100px)"}
+],
+{
+duration:3000+Math.random()*5000,
+iterations:Infinity
+}
+);
+
+container.appendChild(dot);
+}
+
 const toggle=document.getElementById("themeToggle");
 
-if(localStorage.getItem("theme")==="dark"){
-document.body.classList.add("dark");
-}
-
 toggle?.addEventListener("click",()=>{
-
-document.body.classList.toggle("dark");
-
-if(document.body.classList.contains("dark")){
-localStorage.setItem("theme","dark");
-}else{
-localStorage.setItem("theme","light");
-}
-
+document.body.classList.toggle("light");
 });
